987e1bdd28a1f289ec3cf0149f263c8d50e6cb4c
starnawskamarta <starnawskamarta@gmail.com>
2016-10-22 15:36:59 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,50 @@
+# if 3>2:
+#     print('To działa!')
+# if 5>2:
+#     print('5 jest jednak więsze od 2')
+# else:
+#     print('5 nie jest większe od 2')
+# name = 'Marta'
+# if name == 'Ola':
+#     print('Hej Ola!')
+# elif name == 'Marta':
+#     print('Hej Marta!')
+# else:
+#     print('Hej nieznajoma!')
+# glosnosc = 33
+# if glosnosc < 20:
+#     print('Prawie nic nie słychać.')
+# elif 20 <= glosnosc < 40:
+#     print('O, muzyka leci w tle.')
+# elif 40 <= glosnosc < 60:
+#     print ('Idealnie, mogę usłyszeć wszystkie detale.')
+# elif 60 <= glosnosc < 80:
+#     print ('Dobre na imprezy')
+# elif 80 <= glosnosc < 100:
+#     print ('Troszeczkę za głośno!')
+# else:
+#     print ('Ojoj! Moje uszy :(')
+# def hej ():
+#     print('Hej!')
+#     print('Jak się masz?')
+# hej()
+# def hej(imie):
+#     if imie == 'Marta':
+#         print('Hej Marta!')
+#     elif imie == 'Ania':
+#         print('Hej Ania!')
+#     else:
+#         print('Hej nieznajoma')
+# hej('Karol')
+# def hej(imie):
+#     print('Hej ' + imie + '!')
+# hej('Bazyl')
+# def hej (imie):
+#     print('Hej ' + imie + '!')
+#
+# dziewczyny = ['Marta', 'Karol', 'Grzana', "Agatka", 'Ty']
+# for imie in dziewczyny:
+#     hej(imie)
+
+# for i in range (1, 6):
+#     print(i)
